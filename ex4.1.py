469189e1ada0e16cd50a23df8682c0f92d98e114
malihachowdhury6 <144377170+malihachowdhury6@users.noreply.github.com>
2024-02-22 08:50:55 -0600

    Add files via upload

---
+++ ex4.1.py
@@ -0,0 +1,14 @@
+#ex4.1
+
+#Question 1
+
+# Complexity Analysis of Provided Code:
+#The code consists of a function processdata(li) with a nested loop structure where the inner loop doubles the value of li[i] if li[i] > 5.
+
+#Question 2
+#No, they are not the same.
+
+#MODIFIED CODE:
+#def processdata(li):
+    for i in range(len(li)):
+        li[i] = li[i] * 2  # Always double the element
